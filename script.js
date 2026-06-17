c2aabadd3214908b7b187726af1e10a12fbeb4a4
jjonijjoni <24827@daehyun.hs.kr>
2026-06-17 09:07:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ import { getFirestore, collection, addDoc, query, where, orderBy, onSnapshot } f
 
 // 🔥 본인의 Firebase 프로젝트 설정 값으로 다시 채워주세요!
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBwMSX60e_IY3O9wIJf8GWlZnfE6bABldQ",
+  authDomain: "jjonijjoni-4fc75.firebaseapp.com",
+  databaseURL: "https://jjonijjoni-4fc75-default-rtdb.firebaseio.com",
+  projectId: "jjonijjoni-4fc75",
+  storageBucket: "jjonijjoni-4fc75.firebasestorage.app",
+  messagingSenderId: "66509055047",
+  appId: "1:66509055047:web:9b734e794e4556ca07fd62",
+  measurementId: "G-DC3P80HT9H"
 };
 
 const app = initializeApp(firebaseConfig);
